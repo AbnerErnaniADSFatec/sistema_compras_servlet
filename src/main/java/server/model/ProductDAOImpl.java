127bf60d5a451a71c8fa6ca4055b0d54b2536551
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2019-10-30 16:57:09 -0300

    add-data-access-object

--- src/main/java/server/model/ProductDAOImpl.java
+++ src/main/java/server/model/ProductDAOImpl.java
@@ -1,7 +1,13 @@
 package server.model;
-import java.util.List;
 
-public interface ProductDAO {
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class ProductDAOImpl implements ProductDAO{
     public List<Product> getProducts();
     public List<Product> find(String name);
     public Product findByID(Long id);
